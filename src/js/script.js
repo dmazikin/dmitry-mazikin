2d7de2e7c81fab7ad5b79a9da42acc63a1481c00
Dmitry <mazikindima@mail.ru>
2022-05-16 22:14:37 +0300

    add plugins validate

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,25 @@
 const hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
 
 hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
+    menu.classList.add("active");
 });
 closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
+    menu.classList.remove("active");
 });
+
+function validateForm(form) {
+    $(form).validate({
+        messages: {
+            name: "Пожалуйста, введите свое имя",
+            email: {
+                required: "Пожалуйста , введите email",
+                email: "Не правильно введен email",
+            },
+            phone: "Пожалуйста , введите номер телефона",
+            policy: "Пожалуйста, согласитесь с политикой конфиденциальности",
+        },
+    });
+}
+validateForm(".contacts__form");
