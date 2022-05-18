cb3ed4f1dd8f6d8bb2410982b86ff31dab809256
Dmitry <mazikindima@mail.ru>
2022-05-18 13:16:31 +0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,19 @@ function validateForm(form) {
     });
 }
 validateForm(".contacts__form");
+
+$("form").submit(function(e) {
+    e.preventDefault();
+    if (!$(this).valid()) {
+        return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize(),
+    }).done(function() {
+        $(this).find("input").val("");
+        $("form").trigger("reset");
+    });
+    return false;
+});
