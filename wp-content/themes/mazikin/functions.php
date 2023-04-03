929a2d86b8764440b9cf4c04b4344a47208dc1b6
Dmitry <mazikindima@mail.ru>
2023-04-03 12:02:35 +0300

    update

--- wp-content/themes/mazikin/functions.php
+++ wp-content/themes/mazikin/functions.php
@@ -138,14 +138,8 @@ add_action( 'widgets_init', 'mazikin_widgets_init' );
  * Enqueue scripts and styles.
  */
 function mazikin_scripts() {
-	wp_enqueue_style( 'mazikin-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'mazikin-style', 'rtl', 'replace' );
-
+	wp_enqueue_style( 'mazikin-style', get_template_directory_uri(). '/css/style.min.css' );
 	wp_enqueue_script( 'mazikin-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'mazikin_scripts' );
 
