dd917ab25d41c79687975ccd784ce9d05f7bf2f2
Dmitry <mazikindima@mail.ru>
2023-07-12 13:57:08 +0300

    update

--- wp-content/themes/mazikin/header.php
+++ wp-content/themes/mazikin/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -11,13 +12,50 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
+
 <head>
-  <?php wp_head();?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title><?php bloginfo('description'); ?></title>
+  <?php wp_head(); ?>
 </head>
-<body>
-  <h1 class="title">test!!!!</h1>
+
+<body <?php body_class(); ?>>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="b-logo col-lg-2 col-md-3">
+          <span class="b-logo_d">
+            d.
+          </span>
+          <span class="b-logo_m">
+            m
+          </span>
+          <span class="b-logo_4">
+            4
+          </span>
+          <span class="b-logo_z">
+            z
+          </span>
+          <span class="b-logo_i">
+            i
+          </span>
+          <span class="b-logo_k">
+            k
+          </span>
+          <span class="b-logo_i">
+            i
+          </span>
+          <span class="b-logo_n">
+            n
+          </span>
+          <div class="b-logo-web">
+            web-developer
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </header>
