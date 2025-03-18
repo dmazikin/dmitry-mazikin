ef4c0897ce736ec3d5af58ae479fbaa73d58382f
Dmitry <mazikindima@mail.ru>
2025-03-18 11:33:57 +0300

    add: Сделал рабочую секцию портфолио

--- wp-content/themes/mazikin/index.php
+++ wp-content/themes/mazikin/index.php
@@ -255,157 +255,235 @@ get_sidebar();
 <!-- ABOUT -->
 <!-- PORTFOLIO -->
 <article class="portfolio active" data-page="portfolio">
+
   <header>
-    <h2 class="h2 arcticle-title">Портфолио</h2>
+    <h2 class="h2 article-title">Portfolio</h2>
   </header>
+
   <section class="projects">
+
     <ul class="filter-list">
+
       <li class="filter-item">
-        <button class="active" data-filter-btn>Все</button>
+        <button class="active" data-filter-btn>All</button>
       </li>
+
       <li class="filter-item">
-        <button class="active" data-filter-btn>Приложения</button>
+        <button data-filter-btn>Web design</button>
       </li>
+
       <li class="filter-item">
-        <button class="active" data-filter-btn>Разработка</button>
+        <button data-filter-btn>Applications</button>
       </li>
+
+      <li class="filter-item">
+        <button data-filter-btn>Web development</button>
+      </li>
+
     </ul>
+
     <div class="filter-select-box">
+
       <button class="filter-select" data-select>
-        <div class="select-value" data-select-value> Выбрать категорию</div>
+
+        <div class="select-value" data-selecct-value>Select category</div>
+
         <div class="select-icon">
           <ion-icon name="chevron-down"></ion-icon>
         </div>
-        <!-- /.select-icon -->
+
       </button>
-      <!-- /.filter-select -->
+
       <ul class="select-list">
+
         <li class="select-item">
-          <button data-select-item>Все</button>
+          <button data-select-item>All</button>
         </li>
+
         <li class="select-item">
-          <button data-select-item>Приложения</button>
+          <button data-select-item>Web design</button>
         </li>
+
         <li class="select-item">
-          <button data-select-item>Разработка</button>
+          <button data-select-item>Applications</button>
         </li>
+
+        <li class="select-item">
+          <button data-select-item>Web development</button>
+        </li>
+
       </ul>
-      <!-- /.select-list -->
+
     </div>
-    <!-- /.filter-select-box -->
+
     <ul class="project-list">
-      <li class="project-item active" data-filter-item data-category="Разработка">
+
+      <li class="project-item  active" data-filter-item data-category="web development">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
-            <img src="wp-content/themes/mazikin/img/project-1.jpg" alt="Finance" loading="lazy">
+
+            <img src="wp-content/themes/mazikin/img/project-1.jpg" alt="finance" loading="lazy">
           </figure>
+
           <h3 class="project-title">Finance</h3>
-          <p class="project-category">Разработка</p>
+
+          <p class="project-category">Web development</p>
+
         </a>
       </li>
-      <li class="project-item active" data-filter-item data-category="Разработка">
+
+      <li class="project-item  active" data-filter-item data-category="web development">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
-            <img src="wp-content/themes/mazikin/img/project-2.png" alt="Orizon" loading="lazy">
+
+            <img src="wp-content/themes/mazikin/img/project-2.png" alt="orizon" loading="lazy">
           </figure>
+
           <h3 class="project-title">Orizon</h3>
-          <p class="project-category">Разработка</p>
+
+          <p class="project-category">Web development</p>
+
         </a>
       </li>
-      <li class="project-item active" data-filter-item data-category="Разработка">
+
+      <li class="project-item  active" data-filter-item data-category="web design">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
-            <img src="wp-content/themes/mazikin/img/project-3.jpg" alt="Finance" loading="lazy">
+
+            <img src="wp-content/themes/mazikin/img/project-3.jpg" alt="fundo" loading="lazy">
           </figure>
-          <h3 class="project-title">Finance</h3>
-          <p class="project-category">Разработка</p>
-        </a>
-      </li>
-      <li class="project-item active" data-filter-item data-category="Разработка">
-        <a href="#">
-          <figure class="project-img">
-            <div class="project-item-icon-box">
-              <ion-icon name="eye-outline"></ion-icon>
-            </div>
-            <img src="wp-content/themes/mazikin/img/project-4.png" alt="Fundo" loading="lazy">
-          </figure>
+
           <h3 class="project-title">Fundo</h3>
-          <p class="project-category">Разработка</p>
+
+          <p class="project-category">Web design</p>
+
         </a>
       </li>
-      <li class="project-item active" data-filter-item data-category="Приложение">
+
+      <li class="project-item  active" data-filter-item data-category="applications">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
-            <img src="wp-content/themes/mazikin/img/project-5.png" alt="dsm" loading="lazy">
+
+            <img src="wp-content/themes/mazikin/img/project-4.png" alt="brawlhalla" loading="lazy">
           </figure>
-          <h3 class="project-title">dsm</h3>
-          <p class="project-category">Приложение</p>
+
+          <h3 class="project-title">Brawlhalla</h3>
+
+          <p class="project-category">Applications</p>
+
         </a>
       </li>
-      <li class="project-item active" data-filter-item data-category="Приложение">
+
+      <li class="project-item  active" data-filter-item data-category="web design">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
+
+            <img src="wp-content/themes/mazikin/img/project-5.png" alt="dsm." loading="lazy">
+          </figure>
+
+          <h3 class="project-title">DSM.</h3>
+
+          <p class="project-category">Web design</p>
+
+        </a>
+      </li>
+
+      <li class="project-item  active" data-filter-item data-category="web design">
+        <a href="#">
+
+          <figure class="project-img">
+            <div class="project-item-icon-box">
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+
             <img src="wp-content/themes/mazikin/img/project-6.png" alt="metaspark" loading="lazy">
           </figure>
-          <h3 class="project-title">metaspark</h3>
-          <p class="project-category">Приложение</p>
+
+          <h3 class="project-title">MetaSpark</h3>
+
+          <p class="project-category">Web design</p>
+
         </a>
       </li>
-      <li class="project-item active" data-filter-item data-category="Приложение">
+
+      <li class="project-item  active" data-filter-item data-category="web development">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
+
             <img src="wp-content/themes/mazikin/img/project-7.png" alt="summary" loading="lazy">
           </figure>
-          <h3 class="project-title">summary</h3>
-          <p class="project-category">Приложение</p>
+
+          <h3 class="project-title">Summary</h3>
+
+          <p class="project-category">Web development</p>
+
         </a>
       </li>
-      <li class="project-item" data-filter-item data-category="Приложение">
+
+      <li class="project-item  active" data-filter-item data-category="applications">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
+
             <img src="wp-content/themes/mazikin/img/project-8.jpg" alt="task manager" loading="lazy">
           </figure>
-          <h3 class="project-title">task manager</h3>
-          <p class="project-category">Приложение</p>
+
+          <h3 class="project-title">Task Manager</h3>
+
+          <p class="project-category">Applications</p>
+
         </a>
       </li>
-      <li class="project-item" data-filter-item data-category="Приложение">
+
+      <li class="project-item  active" data-filter-item data-category="web development">
         <a href="#">
+
           <figure class="project-img">
             <div class="project-item-icon-box">
               <ion-icon name="eye-outline"></ion-icon>
             </div>
+
             <img src="wp-content/themes/mazikin/img/project-9.png" alt="arrival" loading="lazy">
           </figure>
-          <h3 class="project-title">arrival</h3>
-          <p class="project-category">Приложение</p>
+
+          <h3 class="project-title">Arrival</h3>
+
+          <p class="project-category">Web development</p>
+
         </a>
       </li>
+
     </ul>
-    <!-- /.project-list -->
+
   </section>
-  <!-- /.projects -->
+
 </article>
 <!-- /.portfolio -->
 <!-- PORTFOLIO -->
